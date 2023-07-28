aedc3dc5be6702db54e407da18fc9a2232881584
seema yadav <ramvijayya2004@gmail.com>
2023-07-28 16:41:01 +0530

    Commited

---
+++ lib/models/task_model.dart
@@ -0,0 +1,32 @@
+class Task {
+  int id;
+  String title;
+  DateTime date;
+  String? priority;
+  int? status; // 0 - Incomplete, 1 - Complete
+
+
+  Task({required this.id, required this.title, required  this.date, this.priority, required this.status});
+
+  Map<String, dynamic> toMap() {
+    final map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['title'] = title;
+    map['date'] = date.toIso8601String();
+    map['priority'] = priority;
+    map['status'] = status;
+    return map;
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      title: map['title'],
+      date: DateTime.parse(map['date']),
+      priority: map['priority'],
+      status: map['status'],
+    );
+  }
+}
